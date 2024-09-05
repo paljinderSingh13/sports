df6fdfc300bbc7b26004235942478a2227f5b59b
amandeep9162 <amandeep9162@gmail.com>
2024-09-05 15:46:31 +0530

    club team crud

--- app/Models/Club/Club.php
+++ app/Models/Club/Club.php
@@ -4,9 +4,12 @@ namespace App\Models\Club;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Club extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $fillable = [ 'logo', 'name', 'address', 'city', 'state', 'country', 'postal_code', 'contact_name', 'phone', 'email'];
+
+     protected $dates = ['deleted_at'];
 }
