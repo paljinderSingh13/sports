d02f5743fe46d19041728569ccc73355f60538a2
ergurjeetsingh <ergurjeetsingh11@gmail.com>
2024-09-09 22:31:31 +0530

    Robot font family included

--- resources/views/Components/head.blade.php
+++ resources/views/Components/head.blade.php
@@ -23,6 +23,7 @@
          <link href="{{asset('assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
          <link href="{{asset('assets/libs/simple-datatables/style.css')}}" rel="stylesheet" type="text/css" />
          <link href="{{asset('assets/css/style.css')}}" rel="stylesheet" type="text/css" />
+         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
          
          <style>
           /* .topbar,   .page-content{
--- resources/views/club/login.blade.php
+++ resources/views/club/login.blade.php
@@ -19,6 +19,7 @@
          <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
          <link href="{{asset('assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
          <link href="{{asset('assets/css/style.css')}}" rel="stylesheet" type="text/css" />
+         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
          
 
     </head>
@@ -73,7 +74,7 @@
                                         <div class="form-group mb-0 row">
                                             <div class="col-12">
                                                 <div class="d-grid mt-3">
-                                                    <button class="btn btn-blue btn-lg" type="button">Log In <i class="fas fa-sign-in-alt ms-1"></i></button>
+                                                    <button class="btn btn-primary btn-lg" type="button">Log In <i class="fas fa-sign-in-alt ms-1"></i></button>
                                                 </div>
                                             </div><!--end col--> 
                                         </div> <!--end form-group-->                           
